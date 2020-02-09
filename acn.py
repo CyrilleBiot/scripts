b3f89720d77f1cf9f116c527eacebdbe74440b68
CyrilleBiot <cyrille@cbiot.fr>
2020-02-09 01:34:39 +0100

    'Merge' specific file from devel

--- acn.py
+++ acn.py
@@ -4,14 +4,18 @@
 """ Script d'installation et de configuration du serveur de cache apt
     apt-cacher-ng soit en tant que serveur (ajout du paquet sur le système
     soit en tant que client (cration d'un fichier de proxy apt)
+    Possibilité pour les clients de choisir le port d'écoute du serveur
+    Installation de cron-apt avec configuration spécifique
+    pour installation automatique des mises à jour de sécurité des repo
+    présents sur le système
 """
 
 __author__ = "Cyrille BIOT"
 __copyright__ = "Copyleft"
 __credits__ = "Cyrille BIOT"
 __license__ = "GPL"
-__version__ = "0.2.2"
-__date__ = "2020/01/30"
+__version__ = "2.0.0"
+__date__ = "2020/02/05"
 __maintainer__ = "Cyrille BIOT"
 __email__ = "cyrille@cbiot.fr"
 __status__ = "Devel"
@@ -21,65 +25,80 @@ import nmap, subprocess, socket
 
 
 
+
 def baseDebian():
     """
     Fonction permettant de connaitre le Systeme d'exploitant faisant tourner le script
     Ou DEBIAN ou UBUNTU pour savoir si on utilise su ou sudo
     Retourne une variable de type string (admin)
-    :return: admin soit 'su' soit 'sudo'
+    :return: admin soit 'debian' (root), soit 'ubuntu' (sudo)
     """
     # Ubuntu ou DEBIAN
     if 'Debian' in platform.version():
         # Si DEBIAN, verif si root lance le script
-        admin = 'su'
-        print('Vous utilisez un système Debian (su pour adminstration).')
+        print('Vous utilisez un système Debian (su pour administration).')
         if not os.geteuid() == 0:
-            sys.exit("Seul le root peut lancer ce script. Nécessite privilèges administrateur")
+            sys.exit("Seul le root peut lancer ce script. Nécessite privilèges administrateur.")
+        distrib = 'debian'
     else:
-        print('Vous utilisez un système non Debian (sudo pour adminstration).')
-        admin = 'sudo'
-    return admin
+        if not os.geteuid() == 0:
+            print("Ce programme requiert un lancement via 'sudo'")
+            sys.exit("Ce programme doit être lancé avec les droits administrateur.\nUtiliser sudo LeScript.py")
+        print('Vous utilisez un système non Debian (sudo pour administration).')
+        distrib = 'ubuntu'
+    return distrib
 
-
-def installServeur(ip, port):
+def installPackage(package, debianUbuntu):
     """
-    Fonction installant le serveur de cache apt-cacher-ng
-    :param ip: IP du Serveur
-    :param port: interger port ACN
-    :return:
+    Fonction installant un package debian ou ubuntu
+    :param package: le nom du paquet à installer
+    :param debianUbuntu: soit 'debian' / soit 'ubuntu'
+    :return: None
     """
-    # Installation SERVEUR
-    # Tester si le package apt-cacher-ng est installé ou non
-    retval = subprocess.call(["which", "apt-cacher-ng"])
+    retval = subprocess.call(['which', package])
     if retval != 0:
-        print("Le package apt-cacher-ng n'est pas intallé. Installation...")
+        print("Le package {} n'est pas intallé. Installation...".format(package))
 
         # Paramètres de l'install
-        cmdInstall = ['apt-get', 'install', 'apt-cacher-ng', '-y']
+        cmdInstall = ['apt-get', 'install', package, '-y']
         cmdUpdate = ['apt-get', 'update']
-        # Debian, Ubuntu
-        admin = baseDebian()
 
         # Adaptation système Ubuntu
-        if admin == 'sudo':
+        if debianUbuntu == 'ubuntu':
             cmdInstall.insert(0, 'sudo')
             cmdUpdate.insert(0, 'sudo')
 
         # On installe le paquet
         subprocess.run(cmdInstall)
-        subprocess.run(cmdUpdate)
+        #subprocess.run(cmdUpdate)
     else:
-        print('Le package apt-cacher-ng est déjà présent sur votre système.')
-        sys.exit()
+        print('Le package {} est déjà présent sur votre système.'.format(package))
+
+    return None
+
+def installServeur(ip, port,distrib):
+    """
+    Fonction installant le serveur de cache apt-cacher-ng
+    :param ip: IP du Serveur
+    :param port: interger port ACN
+    :param distrib: Ubuntu ou Debian
+    :return: None
+    """
+
+    # Installation du serveur
+    installPackage('apt-cacher-ng',distrib)
 
     # Affichage Informations
     print("===============================================")
     print("Le serveur de cache est dès lors opérationnel")
-    print("Le port d'écoute est : {}".format(portACN))
-    print("Page d'aministration : http://{}:{}/acng-report.html".format(ipServeur, portACN))
+    print("Le port d'écoute est : {}".format(port))
+    print("Page d'aministration : http://{}:{}/acng-report.html".format(ip, port))
     print("Notez bien l'ip de votre serveur, elle vous sera indispensable pour la configuration des clients.")
-    print("L'IP du serveur est : {} ".format(ipServeur))
+    print("L'IP du serveur est : {} ".format(ip))
     print("Indispensable : cette IP doit être FIXE (réglage sur votre BOX ou serveur DHCP).")
+    print("Cette machine est un serveur, mettre de ne l'arrêter. Les mises à jour s'effectuant la nuit.")
+
+    return None
 
 
 def installClient(ipServeur,portACN):
@@ -88,10 +107,10 @@ def installClient(ipServeur,portACN):
     Créer un fichier dans /etc/apt/apt.conf.d/ ayant pour nom 00aptproxyANC
     :param ipServeur:  ip du serveur ACN
     :param portACN: port d'écoute du serveur ACN
-    :return:
+    :return: None
     """
-    print("Installation client.")
-    # Reste à insérer l'ip et à le coller au bo n endroit
+
+    # COnfig IP serveur dans un fichier de proxy APT
     msgApt = 'Acquire::http::Proxy "http://' + ipServeur + ':' + str(portACN) + '";\n'
     print(msgApt)
     dirInstall = '/etc/apt/apt.conf.d/'
@@ -100,7 +119,31 @@ def installClient(ipServeur,portACN):
     fichier = open(fileLocInstall, "w")
     fichier.write(msgApt)
     fichier.close()
+    return None
 
+
+
+def portSelection(portACN):
+    while True:
+        try:
+            portDefault = input("Utiliser le port par défaut 3142 (recommandé) ?. [Oui / Non]  ")
+            if portDefault.lower() == 'oui':
+                print('Port Serveur {}'.format(portACN))
+                break
+
+            elif portDefault.lower() == 'non':
+                try:
+                    portSelect = int(input("Saisir le port du serveur Apt-Cacher-Ng. Entre 0 et 65 535. : "))
+                    if -1 < portSelect < 65536:
+                        print("Port sélectionné{}".format(portSelect))
+                        portACN = portSelect
+                        break
+                except ValueError:
+                    print("Oops!  Réponse incorrecte, ce n'est pas un nombre compris dans la plage demandée.")
+        except ValueError:
+            print("Oops!  Réponse incorrecte... Réessayer...")
+
+    print("Installation client sur port {}.".format(portACN))
 
 def ipRecuperation():
     """
@@ -234,7 +277,10 @@ def validerIpServeurACN(listIp):
                     ipServeur = listIp[0]
                     break
                 elif ouiNon.lower() == 'non':
-                    sys.exc_info('Revoir la configuration du serveur.\r\n Et relancer ce script.')
+                    sys.exit('Revoir la configuration du serveur.\n'
+                             'Et relancer ce script.\n'
+                             'Aucune machine disponible dans le reseau actuellement '
+                             'avec ce port d\'ouvert')
             except ValueError:
                 print("Oops!  Réponse incorrecte... Réessayer... [Oui / Non ]")
         # Valider l'ip unique
@@ -253,19 +299,78 @@ def validerIpServeurACN(listIp):
                 print("Oops!  Réponse incorrecte... Réessayer...")
     return ipServeur
 
-# ===========================
-# Lancement du script
-# ==========================
-choixInstall = clientServeur()
-if choixInstall.lower() == 'serveur':
-    portACN = 3142
-    ipServeur = ipRecuperation()
-    installServeur(ipServeur,portACN)
-else:
-    # Installation client
-    ip = ipRecuperation()
-    portACN = 3142
 
-    ipServeur = chercherServeurACN(ip, portACN)
-    ipServeur = validerIpServeurACN(ipServeur)
-    installClient(ipServeur,portACN)
+def installCronApt(distrib):
+    """
+    Fonction Recuperation des entrées des mises à jour de sécurité dans
+     dans les divers sources.list possibles
+     Et création d'un sources.list basé que sur ces entrées (security)
+     Le fichier est propre à primtux. Donc si existe, on le régénère sinon on le crée
+     Et envoi mail sur root
+    :param distrib: Ubuntu ou Debian
+    :return: None
+    """
+
+    mailRoot = 'root'
+    aptSecurity = "find /etc/apt -type f -name '*.list' " \
+                  "| xargs cat " \
+                  "|  grep -v \"^#\" | grep security"
+
+    # Installation de cron-apt
+    installPackage('cron-apt',distrib)
+
+    # Création sources.list spécial sécurité
+    log = open('/etc/apt/sources.list.d/security-primtuxACN.list', 'w')
+    log.write('# Security Update. For Primtux Apt-cacher-ng.\n')
+    log.flush()
+    c = subprocess.call(aptSecurity, stdout=log, stderr=log, shell=True)
+
+    # Configuration d'une action dans la conf de cron-apt
+    # /etc/cron-apt/action.d/5-primtuxACN-security
+    fichier = open('/etc/cron-apt/action.d/5-primtuxACN-security', "w")
+    fichier.write("upgrade -y -o APT::Get::Show-Upgraded=true\n")
+    fichier.write("OPTIONS=\"-o quiet=1 -o APT::Get::List-Cleanup=false -o "
+                  "Dir::Etc::SourceList=/etc/apt/sources.list.d/security-primtuxACN.list "
+                  "-o Dir::Etc::SourceParts=\\\"/dev/null\\\"\"\n")
+    fichier.write("MAILTO=\"{}\"\n".format(mailRoot))
+    fichier.write("MAILON=\"always\"\n")
+    fichier.close()
+
+    print("Dès lors, le système installera les mises à jour de sécurité, toutes les nuits  à 4 heures.")
+
+    return None
+
+
+
+def main():
+    """
+    Lancement du script
+    :return: None
+    """
+    # Défnition du port par defaut d'ACN
+    portACN = 3142
+    # Recupere le type de distribution faisant tourner le script
+    distrib  = baseDebian()
+
+    choixInstall = clientServeur()
+    if choixInstall.lower() == 'serveur':
+        ipServeur = ipRecuperation()
+        installServeur(ipServeur, portACN, distrib)
+    else:
+        # Installation client
+        portSelection(portACN)
+        ip = ipRecuperation()
+        ipServeur = chercherServeurACN(ip, portACN)
+        ipServeur = validerIpServeurACN(ipServeur)
+        installClient(ipServeur,portACN)
+
+    # Que ce sont pour l'un ou l'autre, install cron-apt auto securité
+    installCronApt(distrib)
+    return None
+
+"""
+ Boucle main()
+"""
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
